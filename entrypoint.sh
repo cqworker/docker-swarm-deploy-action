9e68afc3ad85dec86e866ad1fd38289900d7c9bb
cqworker <29402297+cqworker@users.noreply.github.com>
2020-09-02 18:16:08 +0800

    Update entrypoint.sh
    使用action 出现 Error loading key "/github/home/.ssh/docker": invalid format

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,11 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
 
     # Save private key to a file and register it with the agent.
     mkdir -p "$HOME/.ssh"
+    echo $INPUT_SSH_PRIVATE_KEY
+    echo "------"
+    echo "$INPUT_SSH_PRIVATE_KEY"
+    cat $HOME/.ssh/docker
+    cat "$HOME/.ssh/docker"
     printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
     chmod 600 "$HOME/.ssh/docker"
     eval $(ssh-agent)
