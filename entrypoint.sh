f8eb47ae22cf8d7ce0eebc397d8e396df7c7a0ff
cqworker <29402297+cqworker@users.noreply.github.com>
2020-09-03 12:22:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,6 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
     # Save private key to a file and register it with the agent.
     mkdir -p "$HOME/.ssh"
     printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
-    echo "------------"
-    cat $HOME/.ssh/docker
     chmod 600 "$HOME/.ssh/docker"
     eval $(ssh-agent)
     ssh-add "$HOME/.ssh/docker"
@@ -37,4 +35,4 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
 fi
 
 echo "Connecting to $INPUT_REMOTE_HOST..."
-docker --log-level debug --host "$INPUT_REMOTE_HOST" "$@" 2>&1
+docker --log-level debug --host 47.108.195.29 
